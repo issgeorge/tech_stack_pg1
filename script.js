246b52d9817f8731b296668c620d539003179794
Ignatius Stanis Santosh <63264790+issgeorge@users.noreply.github.com>
2025-07-12 21:14:36 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const text = "Currently building my tech tool kit.";
+const speed = 50;
+let i = 0;
+
+function typeWriter() {
+  if (i < text.length) {
+    document.getElementById("typewriter").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+window.onload = typeWriter;
